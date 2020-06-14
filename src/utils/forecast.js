154ffdb4c9da32393a520799dbcecb66d6fcf9f1
Chenhao Lin <spellcasters000@gmail.com>
2020-06-14 18:34:19 +0800

    Remove unnecessary forecast output

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,24 +1,24 @@
-const request = require('request');
+const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
   const url =
     'http://api.weatherstack.com/current?access_key=18cd2203e47b8ffdb3eb8c704276d799&query=' +
     latitude +
     ',' +
-    longitude;
+    longitude
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback('Unable to connect to location services.', undefined);
+      callback('Unable to connect to location services.', undefined)
     } else if (body.error) {
-      callback('Unable to find location.Try another search.', undefined);
+      callback('Unable to find location.Try another search.', undefined)
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions}. \nIt is currently ${body.current.temperature} degrees out. \nIt feels like ${body.current.feelslike} degrees out. \nThe humidity is ${body.current.humidity} percent.`
-      );
+        `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity} percent.`
+      )
     }
-  });
-};
+  })
+}
 
-module.exports = forecast;
+module.exports = forecast
